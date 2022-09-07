5ff202313acae3a7cac738d8ad51317264f6c9aa
nnnLik <makhmudov_ra@vk.com>
2022-09-07 13:28:38 +0300

    feat several page for output information about model-coin, about-coin, list

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import requests
-import schemas
-
+from schemas import Converter
 from fastapi import FastAPI
+
 
 app = FastAPI()
 
+a = {'id': 'qwe', 'symbol': 'qwewee', 'snt_new': 123}
+
 
 @app.get("/{coin}")
-async def main(coin: str):
-    return requests.get(f'https://api.coingecko.com/api/v3//coins/list').json()
+async def test_model(coin: str):
+    user_coin = requests.get(f'https://api.coingecko.com/api/v3//coins/{coin}').json()
+    output = Converter(**user_coin)
+    return output
+
+
+@app.get("/about_coin/{coin}")
+async def about_coin(coin: str):
+    user_coin = requests.get(f'https://api.coingecko.com/api/v3//coins/{coin}').json()
+    return user_coin
+
+
+@app.get("/list_of_coins")
+async def list_of_coins():
+    user_coin = requests.get(f'https://api.coingecko.com/api/v3//coins/list').json()
+    return user_coin
